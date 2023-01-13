30c91c6d7eaaead8e0e360d816ccdb54879014a4
wenned <wenned@gmail.com>
2023-01-13 15:20:06 -0300

    preparacao para recebimento API

--- servidor.js
+++ servidor.js
@@ -13,60 +13,6 @@ var urlencodedParser = bodyParser.urlencoded({ extended: true });
 
 app.use(express.static(__dirname + '/public'));
 
-
-// Configuracoes com banco de dados: ======================
-
-const db = require('./public/javascript/conect.js');
-const { notEqual } = require('assert');
-const { json, query } = require('express');
-const { Console } = require('console');
-
-
-async function updateValue(x){
-
-    var un = "select * from produtos where nome_prod='"+x[0]+"';"
-
-    await db.connect()
-    await db.query(un, (err, res) => {
-        if (err) {
-            console.log(err.stack)
-
-            db.end()
-
-        } else {
-
-			var quantidade = res.rows[0]['quantidade'];
-
-			if (quantidade > 0){
-
-				db.query(`update produtos set quantidade=${x[1]} where nome_prod='${x[0]}';`)
-
-			db.end()};
-
-        };
-    });
-    
-};
-
-
-async function UpdatePrice (){
-
-	var consulta = "select * from preco;"
-
-	await db.connect()
-	await db.query(consulta, (err, res) =>{
-		if (err){
-			console.log('erro de retorno de dados '+ err)
-		}else{
-			
-			console.log( JSON.stringify(res.rows))
-
-
-			db.end()
-		}
-	})
-}
-
 // /==============================================================/ 
 
 
@@ -79,20 +25,6 @@ var servidor = app.listen(8080, function(){
 app.get('/', urlencodedParser, function (req, res) {
 
 	fs.readFile('index.html', function(erro, dad){
-
-		// var consulta = "select * from preco;"
-
-		//  db.connect()
-		//  db.query(consulta, (err, res) =>{
-		// 	if (err){
-		// 		console.log('erro de retorno de dados '+ err)
-		// 	}else{
-				
-		// 		dados = JSON.stringify(res.rows)
-	
-		// 		db.end()
-		// 	}
-		// })
 
 		res.writeHead(200, {'Content-Type': 'text/html'});
 		res.write(dad);
