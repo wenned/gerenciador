b2e59191329dc5120cb0a3cb0f0849a9dc85c311
wenned <wenned@gmail.com>
2023-04-04 23:25:12 -0300

    atualizacao area de fechamento caixa

--- food_ecommerce/src/area_Adm/components/Fechar.js
+++ food_ecommerce/src/area_Adm/components/Fechar.js
@@ -3,6 +3,41 @@ import { Link } from 'react-router-dom';
 import style from './Styles/Fechar.module.css'
 import Logo from '../../componentes/Logo'
 
+async function fecharCaixa(...args){
+    
+    var DADOS_FECHAMENTO =[{
+        "Nome":"",
+        "Data":"",
+        "Itens":"",
+        "Valor_Lancamento":"",
+        "Valor_Total":""
+    }]
+
+    const [itens, data, ValorTotal, nome, saldodia] = args
+
+    DADOS_FECHAMENTO[0]['Nome'] = nome
+    DADOS_FECHAMENTO[0]['Data'] = data
+    DADOS_FECHAMENTO[0]['Itens'] = itens
+    DADOS_FECHAMENTO[0]['Valor_Lancamento'] = ValorTotal
+    DADOS_FECHAMENTO[0]['Valor_Total'] = saldodia
+
+    try {
+        const RETORNO_DADOS = await fetch(`http://192.168.31.3:8080/fechamento_caixa/`, {
+            method: `POST`,
+            body: JSON.stringify(DADOS_FECHAMENTO[0]),
+            headers: {"Content-type": "application/json; charset=UTF-8"}
+        });
+        
+        const DATA = await RETORNO_DADOS.json();
+        return DATA
+
+    } catch (error) {
+        console.log('Deu erro aqui ',error)
+    }
+
+}
+
+
 function Fechar(){
     
     const [DADOS_RETORNO, setDADOS_RETORNO] = useState(new Date().toISOString().slice(0,10))
@@ -12,12 +47,14 @@ function Fechar(){
     const [cache, setCache] = useState('')
     const [card, setCard] = useState('')
     const [pix, setPix] = useState('')
+    const [nome , setNome] = useState('')
 
 
     const dateInputRef = useRef(null)
     const cacheInputRef = useRef(null)
     const cardInputRef = useRef(null)
     const pixInputRef = useRef(null)
+    const nomeInputRef = useRef(null)
 
     const Valor = cache+card+pix
 
@@ -37,8 +74,11 @@ function Fechar(){
                         case 'date':
                             setDate(e.target.value)
                             break
-                            default:
+                            case 'nome':
+                                setNome(e.target.value)
                                 break
+                                default:
+                                    break
 
         }
     };
@@ -82,7 +122,7 @@ function Fechar(){
             <span className={style.line}></span>
             <div className={style.lancamento}>
 
-                <form action="/?">
+                <form >
                     <div className={style.cabecario}>Lancamento de Entradas</div>
 
                     <input id='date' type='date' onChange={handleChanger} ref={dateInputRef} className={style.butt}></input>
@@ -90,12 +130,12 @@ function Fechar(){
                     <input id='cache' onChange={handleChanger} ref={cacheInputRef} className={style.butt} placeholder='Dinheiro'></input>
                     <input id='card' onChange={handleChanger} ref={cardInputRef} className={style.butt} placeholder='Cartao'></input>
                     <input id='pix' onChange={handleChanger} ref={pixInputRef} className={style.butt} placeholder='Pix'></input>
-                    <button type='submit' className={style.butt} >Fechar Caixa</button>
+                    <button type='submit' className={style.butt} onClick={()=>fecharCaixa(DADOS_RETORNO, date, Valor, nome, SALDO_TOTAL_DIA)}>Fechar Caixa</button>
 
                     <div className={style.saldoTotal}>Saldo Total: R$ {SALDO_TOTAL_DIA.toFixed(2)}</div>
                     <div className={style.saldo}>Total Lancamento: R$ {Valor}</div>
                     
-                    <input placeholder='Digite seu nome' className={style.butt}></input>
+                    <input id='nome' onChange={handleChanger} ref={nomeInputRef} placeholder='Digite seu nome' className={style.butt}></input>
 
 
                 </form>
