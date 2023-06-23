00f671528da02bd940a56ec70b4b9da6011ffca4
wenned <wenned@gmail.com>
2023-06-23 08:08:41 -0300

    refatoramento de componente

--- food_ecommerce/src/area_Cliente/Quantidade.js
+++ food_ecommerce/src/area_Cliente/Quantidade.js
@@ -5,163 +5,8 @@ import { useState } from 'react';
 // FUNCIONALIDADES
 
 import {adicionar_adicional} from './Funcionalidades/adicional'
-
-// IMAGENS PASTEIS
-import carne from './imagens/carne.png'
-import queijo from './imagens/Queijo.png'
-import carnequeijo from './imagens/CarneQueijo.png'
-import palmitoqueijo from './imagens/palmitoqueijo.png'
-import bananaqueijochocolate from './imagens/bananaqueijochocolate.png'
-import calabresa from './imagens/calabresa.png'
-import frangoqueijo from './imagens/frangoqueijo.png'
-import frangocheddar from './imagens/frangocheddar.png'
-import frango from './imagens/frango.png'
-import carnesol from './imagens/carnesol.png'
-import carnesolqueijo from './imagens/carnesolqueijo.png'
-import presuntoqueijo from './imagens/presuntoqueijo.png'
-import carnecheddar from './imagens/carnecheddar.png'
-import carnecatupiry from './imagens/carnecatupiry.png'
-import carnebacon from './imagens/carnebacon.png'
-import frangocatupiry from './imagens/frangocatupiry.png'
-import frangopalmito from './imagens/frangopalmito.png'
-import frangocalabresa from './imagens/frangocalabresa.png'
-import frangobacon from './imagens/frangobacon.png'
-import calabresaqueijo from './imagens/calabresaqueijo.png'
-import carnesolbacon from './imagens/carnesolbacon.png'
-import carnesolcheddar from './imagens/carnesolcheddar.png'
-import carnesolcatupiry from './imagens/carnesolcatupiry.png'
-import bananaqueijocanelap from './imagens/bananaqueijocanelap.png'
-import bananachocolate from './imagens/bananachocolate.png'
-import chocolatequeijo from './imagens/chocolatequeijo.png'
-import goiabadaqueijo from './imagens/goiabadaqueijo.png'
-
-
-//IMAGENS FRANCES
-
-import pizzaf from './imagens/pizzaf.png'
-import palmitoqueijof from './imagens/palmitoqueijof.png'
-import frangocatupiryf from './imagens/frangocatupiryf.png'
-import bananaqueijocanelaf from './imagens/bananaqueijocanelaf.png'
-import bananaqueijochocolatef from './imagens/bananaqueijochocolatef.png'
-import goiabadaqueijof from './imagens/goiabadaqueijof.png'
-import frangocalabresaf from './imagens/frangocalabresaf.png'
-import frangopalmitof from './imagens/frangopalmitof.png'
-import frangocheddarf from './imagens/frangocheddarf.png'
-import frangobaconf from './imagens/frangobaconf.png'
-import carnesolqueijof from './imagens/carnesolqueijof.png'
-import carnesolcheddarf from './imagens/carnesolcheddarf.png'
-import carnesolcatupiryf from './imagens/carnesolcatupiryf.png'
-import carnesolbaconf from './imagens/carnesolbaconf.png'
-import frangoazeitonaf from './imagens/frangoazeitonaf.png'
-import carnesolazeitonaf from './imagens/carnesolazeitonaf.png'
-import carnesolpalmitof from './imagens/carnesolpalmitof.png'
-import carnesolbananaf from './imagens/carnesolbananaf.png'
-
-// BEBIDAS
-
-import laranja from './imagens/laranja.png'
-import acerola from './imagens/acerola.png'
-import maracuja from './imagens/maracuja.png'
-import cocalata from './imagens/cocalata.png'
-import fantalata from './imagens/fantalata.png'
-import pepsilata from './imagens/pepsilata.png'
-import guaranalata from './imagens/guaranalata.png'
-import guaranalitro from './imagens/guaranalitro.png'
-import cocalitro from './imagens/cocalitro.png'
-import fantalitro from './imagens/fantalitro.png'
-import agua from './imagens/agua.png'
-import aguagas from './imagens/aguagas.png'
-
-
-// IMAGENS SUICO
-
-import salsichaqueijos from './imagens/salsichaqueijos.png'
-
-    const Pastel = {
-      "Carne": carne,
-      "Queijo": queijo,
-      "Carne-Queijo": carnequeijo,
-      "Palmito-Queijo": palmitoqueijo,
-      "Banana-Queijo-Chocolate": bananaqueijochocolate,
-      "Calabresa": calabresa,
-      "Frango-Queijo": frangoqueijo,
-      "Frango-Cheddar": frangocheddar,
-      "Frango": frango,
-      "Carnesol": carnesol,
-      "Carnesol-Queijo": carnesolqueijo,
-      "Presunto-Queijo": presuntoqueijo,
-      "Carne-Cheddar": carnecheddar,
-      "Carne-Catupiry": carnecatupiry,
-      "Carne-Bacon": carnebacon,
-      "Frango-Catupiry": frangocatupiry,
-      "Frango-Palmito": frangopalmito,
-      "Frango-Calabresa": frangocalabresa,
-      "Frango-Bacon": frangobacon,
-      "Calabresa-Queijo": calabresaqueijo,
-      "Carnesol-Bacon": carnesolbacon,
-      "Carnesol-Cheddar": carnesolcheddar,
-      "Carnesol-Catupiry": carnesolcatupiry,
-      "Banana-Queijo-Canela": bananaqueijocanelap,
-      "Banana-Chocolate": bananachocolate,
-      "Chocolate-Queijo": chocolatequeijo,
-      "Goiabada-Queijo": goiabadaqueijo,
-    }
-
-    const Frances = {
-      "Pizza": pizzaf,
-      "Palmito-Queijo": palmitoqueijof,
-      "Frango-Catupiry": frangocatupiryf,
-      "Frango-Palmito": frangopalmitof,
-      "Frango-Calabresa": frangocalabresaf,
-      "Frango-Bacon": frangobaconf,
-      "Frango-Cheddar": frangocheddarf,
-      "Frango-Azeitona": frangoazeitonaf,
-      "Carnesol-Bacon": carnesolbaconf,
-      "Carnesol-Queijo": carnesolqueijof,
-      "Carnesol-Palmito": carnesolpalmitof,
-      "Carnesol-Banana": carnesolbananaf,
-      "Carnesol-Azeitona": carnesolazeitonaf,
-      "Carnesol-Cheddar": carnesolcheddarf,
-      "Carnesol-Catupiry": carnesolcatupiryf,
-      "Banana-Queijo-Canela": bananaqueijocanelaf,
-      "Banana-Queijo-Chocolate": bananaqueijochocolatef,
-      "Goiabada-Queijo": goiabadaqueijof
-    }
-
-    const Suico = {
-
-      "Frango-Catupiry": frangocatupiry,
-      "Frango-Queijo": frangoqueijo,
-      "Frango-Cheddar": frangocheddar,
-      "Frango": frango,
-      "Presunto-Queijo": presuntoqueijo,
-      "Calabresa-Queijo": calabresaqueijo,
-      "Carnesol-Queijo": carnesolqueijo,
-      "Carnesol": carnesol,
-      "Salsicha-Queijo": salsichaqueijos,
-      "Chocolate": carne,
-      "Queijo": queijo,
-      "Carnesol-Cheddar": carnesolcheddar,
-      "Carnesol-Catupiry": carnesolcatupiry,
-      "Chocolate-Queijo": chocolatequeijo,
-      "Goiabada-Queijo": goiabadaqueijo,
-
-    }
-
-    const Bebida ={
-      "Laranja": laranja,
-      "Maracuja": maracuja,
-      "Acerola": acerola,
-      "Agua": agua,
-      "AguaGas": aguagas,
-      "CocaLata": cocalata,
-      "FrantaLata": fantalata,
-      "GuaranaLata": guaranalata,
-      "PepsiLata": pepsilata,
-      "CocaLitro": cocalitro,
-      "FantaLitro": fantalitro,
-      "GuaranaLitro": guaranalitro
-    }
+import { addValores } from './Funcionalidades/adicionarValores';
+import { Pastel, Frances, Suico, Bebida } from './Funcionalidades/imagensItens';
 
 const Item = ['Queijo','Cheddar','Catupiry','Chocolate','Carne','Carnesol','Bacon','Frango','Azeitona','Goiabada','Palmito','Calabresa','Presunto','Canela',]
 
@@ -213,111 +58,6 @@ function Quantidade(){
         });
     }
 
-    function addValores(){
-
-      var reload;      
-      reload = dado
-
-      if(localStorage.getItem('Pedido') === null){
-
-        reload['Itens'].forEach((element, index)=> {
-          if(element['Item']['Valor'].length === 0){
-            reload['Itens'][index]['Item']['Quantidade'] = Valor
-
-            var menu_valores;
-  
-            switch(element['Item']['Tipo']){
-  
-              case 'Pastel':
-                menu_valores = JSON.parse(localStorage.getItem('menu_pasteis'))
-                break
-  
-                case 'Frances':
-                  menu_valores = JSON.parse(localStorage.getItem('menu_frances'))
-                  break
-  
-                  case 'Suico':
-                    menu_valores = JSON.parse(localStorage.getItem('menu_suicos'))
-                    break
-  
-                    case 'Bebida':
-                      menu_valores = JSON.parse(localStorage.getItem('menu_bebidas'))
-                      break
-  
-                      case 'Hamburguer':
-                        menu_valores = JSON.parse(localStorage.getItem('menu_hamburgues'))
-                        break
-  
-                        default:
-                          break
-            };
-  
-            for (item in menu_valores){
-              var verf = element['Item']['Sabor'] in menu_valores[item]
-              
-              if(verf === true){
-              var valorItem = Object.values(menu_valores[item][element['Item']['Sabor']])
-  
-                reload['Itens'][index]['Item']['Valor'] = valorItem[0]
-                reload['Itens'][index]['Item']['Sabor'] = reload['Itens'][index]['Item']['Sabor'].split("-")
-                localStorage.setItem('Modelo', JSON.stringify(reload))
-  
-              };
-            };
-          };
-        });
-        
-      }else{
-        
-        reload['Itens'].forEach((element, index)=> {
-          if(element['Item']['Valor'].length === 0){
-            reload['Itens'][index]['Item']['Quantidade'] = Valor
-            
-            var menu_valores;
-  
-            switch(element['Item']['Tipo']){
-  
-              case 'Pastel':
-                menu_valores = JSON.parse(localStorage.getItem('menu_pasteis'))
-                break
-  
-                case 'Frances':
-                  menu_valores = JSON.parse(localStorage.getItem('menu_frances'))
-                  break
-  
-                  case 'Suico':
-                    menu_valores = JSON.parse(localStorage.getItem('menu_suicos'))
-                    break
-  
-                    case 'Bebida':
-                      menu_valores = JSON.parse(localStorage.getItem('menu_bebidas'))
-                      break
-  
-                      case 'Hamburguer':
-                        menu_valores = JSON.parse(localStorage.getItem('menu_hamburgues'))
-                        break
-  
-                        default:
-                          break
-            };
-  
-            for (item in menu_valores){
-              var verf = element['Item']['Sabor'] in menu_valores[item]
-              
-              if(verf === true){
-              var o = Object.values(menu_valores[item][element['Item']['Sabor']])
-  
-                reload['Itens'][index]['Item']['Valor'] = o[0]
-                reload['Itens'][index]['Item']['Sabor'] = reload['Itens'][index]['Item']['Sabor'].split("-")
-                localStorage.setItem('Modelo', JSON.stringify(reload))
-  
-              };
-            };
-          };
-        });
-      };
-    };
-
     var IMAGEM = Pastel
 
     switch(tip){
@@ -336,7 +76,7 @@ function Quantidade(){
               default:
                 break
     };
-
+console.log(typeof(Valor_Atual))
     return (
         <section className={style.conteiner}>
         
@@ -389,7 +129,7 @@ function Quantidade(){
 
           <div className={style.adicionarNovo}>
 
-              <Link onClick={addValores} to={`/tipo/${MESAKEY[1]['Mesa']}/newitem`}>
+              <Link onClick={()=>{addValores(valor_item, Valor)}} to={`/tipo/${MESAKEY[1]['Mesa']}/novoItem`}>
                   <div className={style.Butao}>Adicionar Novo Item</div>
               </Link>
 
@@ -398,7 +138,7 @@ function Quantidade(){
           <div className={style.final}>
 
               <Link to='/finalizar'>
-                  <div onClick={()=>{ addValores()}} className={style.Butao}>Finalizar</div>
+                  <div onClick={()=>{ addValores(Valor_Atual, Valor)}} className={style.Butao}>Finalizar</div>
               </Link>
               
           </div>
