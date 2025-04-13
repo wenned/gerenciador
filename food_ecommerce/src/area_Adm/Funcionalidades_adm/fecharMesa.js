7be60ad4030c3d0f652a07eab0541f4814a1c6da
exprox <wenned@gmail.com>
2025-04-13 16:10:41 -0300

    implementacao o metodo fecthMesas.

--- food_ecommerce/src/area_Adm/Funcionalidades_adm/fecharMesa.js
+++ food_ecommerce/src/area_Adm/Funcionalidades_adm/fecharMesa.js
@@ -11,3 +11,36 @@ export async function fecharMesa(NMesa){
 
     return Resposta
 };
+
+
+
+export async function fecthMesas(...args){
+
+    const user = process.env.REACT_APP_COUCHDB_USER;
+    const pass = process.env.REACT_APP_COUCHDB_PASSWORD;
+
+    if (!user || !pass) {
+        throw new Error('Credenciais do CouchDB não definidas nas variáveis de ambiente.');
+    }
+
+    const auth = btoa(`${user}:${pass}`);
+
+    
+    const response = await fetch(`${process.env.REACT_APP_COUCHDB_URL}/${args[0]}/_all_docs?include_docs=true`, {
+        method: 'GET',
+        headers: {
+            'Authorization': `Basic ${auth}`,
+            'Content-Type': 'application/json',
+        },
+        credentials: 'include',
+    });
+
+    if (!response.ok) {
+        throw new Error(`Erro ao buscar dados: ${response.status}`);
+    }
+
+    const data = await response.json();
+    const formattedData = data.rows.map(row => row.doc);
+
+    return formattedData;
+}
